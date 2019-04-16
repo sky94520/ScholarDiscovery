226a22fe9a96deca59970657165ddb5e75614d27
sky94520 <541052067@qq.com>
2019-04-16 18:57:06 +0800

    By <xiaoniu> 修改了主页的重定向，目前已经能先搜学校，之后精确到学院，之后到个人的一条线

--- MicroFlask/app.py
+++ MicroFlask/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect
+from flask import url_for
 from nameko.standalone.rpc import ClusterRpcProxy
 import json
 
@@ -10,17 +12,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    return redirect(url_for("institution_info", school_name="清华大学", institution_name="理学院"))
 
-    return "hello world"
 
-
-@app.route('/teachers/<school_name>/<institution_name>')
-def teacher_info(school_name, institution_name):
+@app.route('/institution/<school_name>/<institution_name>')
+def institution_info(school_name, institution_name):
     with ClusterRpcProxy(CONFIG) as rpc:
         # 学校名 => 学校ID
         info = rpc.school.get_id_by_school_name(school_name, institution_name)
         if len(info) == 0:
             return "学校名或者学院名输入有误"
+
         school_id = info['school_id']
         institution_id = info['institution_id']
         # 获取该学院的带有头衔的所有老师
@@ -29,20 +31,58 @@ def teacher_info(school_name, institution_name):
         teachers = json.loads(result)
 
         # 学校代码信息
-        result = rpc.school.get_discipline_by_institution(institution_id)
+        result = rpc.school.get_disciplines_by_institutions(institution_id)
         disciplines = json.loads(result)
+        discipline_mapping = {}
+        # 获取学科的id数组
+        if len(disciplines) > 0:
+            discipline_ids = []
+            for discipline in disciplines[str(institution_id)]:
+                discipline_ids.append(discipline["code"])
+            # 获取学科对应的名称
+            json_str = rpc.school.get_names_by_discipline_ids(discipline_ids)
+            discipline_mapping = json.loads(json_str)
+
+    return render_template("institution_info.html",
+                           institutions=disciplines,
+                           institution_name=institution_name,
+                           teachers=teachers,
+                           discipline_mapping=discipline_mapping)
+
+
+@app.route("/school/<school_name>")
+def school_info(school_name):
+    with ClusterRpcProxy(CONFIG) as rpc:
+        # 获取该学校的学院信息
+        json_str = rpc.school.get_institutions_by_school(school_name)
+        institutions = json.loads(json_str)
+        # 获取学院的id,并转化为数组
+        institution_ids = [institution['id'] for institution in institutions]
+
+        # 获取学院的学科信息
+        json_str = rpc.school.get_disciplines_by_institutions(institution_ids)
+        disciplines = json.loads(json_str)
+
         # 获取学科的id数组
         discipline_ids = []
-        for discipline in disciplines[str(institution_id)]:
-            discipline_ids.append(discipline["discipline_code"])
+        for institution_id in institution_ids:
+            if institution_id in disciplines:
+                for discipline in disciplines[institution_id]:
+                    discipline_ids.append(discipline["code"])
+            else:
+                print("%s 没有对应的学科信息" % institution_id)
         # 获取学科对应的名称
         json_str = rpc.school.get_names_by_discipline_ids(discipline_ids)
         discipline_mapping = json.loads(json_str)
 
-    return render_template("teachers.html",
-                           institutions=disciplines,
-                           teachers=teachers,
-                           discipline_mapping=discipline_mapping)
+        if len(institutions) != 0:
+            return render_template("school_info.html",
+                                   school_name=school_name,
+                                   institutions=institutions,
+                                   disciplines=disciplines,
+                                   discipline_mapping=discipline_mapping)
+        else:
+            return "暂时没有该学校的相关信息"
 
 
 @app.route("/teacher/<teacher_name>/<int:teacher_id>")
@@ -50,7 +90,6 @@ def teacher_detail(teacher_name, teacher_id):
     with ClusterRpcProxy(CONFIG) as rpc:
         # 获取该老师的所有论文
         json_str = rpc.school.get_papers_by_author_id(teacher_id)
-
         papers = json.loads(json_str)
         titles = "{}"
 
